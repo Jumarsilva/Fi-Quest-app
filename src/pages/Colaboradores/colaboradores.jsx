3e0bc81fb98d02941ba33537ec774db239867c30
MariaHRCastro <mariahribeiro@protonmail.com>
2023-04-03 21:21:49 -0300

    users e questions

--- src/pages/Colaboradores/colaboradores.jsx
+++ src/pages/Colaboradores/colaboradores.jsx
@@ -2,6 +2,7 @@ import { Avatar, Space, Table, Typography } from "antd";
 import AppFooter from "../../components/AppFooter/AppFooter";
 import AppHeader from "../../components/AppHeader/AppHeader";
 import SideMenu from "../../components/SideMenu/SideMenu";
+import users from "../../data/users";
 
 import './colaboradores.css'
 
@@ -16,8 +17,7 @@ function Colaboradores() {
                      <Space size={20} direction="vertical">
                     <Typography.Title level={4}>Colaboradores</Typography.Title>
                     <Table
-
-
+                    dataSource={users}
                         columns={[
                             {
                                 title: "Foto",
@@ -52,7 +52,8 @@ function Colaboradores() {
                         pagination={{
                             pageSize: 5,
                         }}
-                    ></Table>
+                    >
+                    </Table>
                 </Space>
                 </div>
                
